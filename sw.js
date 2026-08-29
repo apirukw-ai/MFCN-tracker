715473fa428016a53052b1488bb69d3511264967
apirukw-ai <apirukw@gmail.com>
2026-08-29 22:56:12 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -3,10 +3,6 @@ const ASSETS_TO_CACHE = [
   './',
   './index.html',
   './manifest.json',
-  'https://cdn.tailwindcss.com',
-  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css',
-  'https://fonts.googleapis.com/css2?family=Prompt:wght@300;400;500;600;700&display=swap',
-  'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2'
 ];
 
 self.addEventListener('install', (event) => {
